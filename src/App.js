68320c4a92e91e793ec4f7fd23bcaa76ce80ad3e
Lewis Horwood <horwood.lewis@gmail.com>
2019-05-21 12:21:52 +0900

    style basic page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 2rem;
+  width: 100vw;
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`
 
 const App = () => (
-  <div>
+  <Wrapper>
     <header>
       <h1>Meteorita</h1>
     </header>
     <main>
 
     </main>
-  </div>
+  </Wrapper>
 )
 
 export default App;
